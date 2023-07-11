75e566bb4b23da15b28493da50322be7bb71acdf
이다영 <yooung513@gmail.com>
2023-07-11 16:37:58 +0900

    비밀번 유효성 검증기_테스트코드 작성

--- mvc_practice/src/main/java/org/example/WebApplicationServer.java
+++ mvc_practice/src/main/java/org/example/WebApplicationServer.java
@@ -1,19 +1,25 @@
 package org.example;
 
-// Shift을(를) 두 번 눌러 전체 검색 대화상자를 열고 'show whitespaces'를 입력한 다음,
-// Enter를 누르세요. 그러면 코드 내에서 공백 문자를 확인할 수 있습니다.
-public class Main {
-    public static void main(String[] args) {
-        // 캐럿을 강조 표시된 텍스트에 놓고 Opt+Enter을(를) 누르면
-        // IntelliJ IDEA의 수정 제안을 볼 수 있습니다.
-        System.out.printf("Hello and welcome!");
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-        // Ctrl+R을(를) 누르거나 여백의 녹색 화살표 버튼을 클릭하여 코드를 실행합니다.
-        for (int i = 1; i <= 5; i++) {
+import java.io.File;
 
-            // Ctrl+D을(를) 눌러 코드 디버그를 시작합니다. 중단점을 하나 설정해 드렸습니다.
-            // 중단점을 더 추가하려면 언제든지 Cmd+F8을(를) 누르세요.
-            System.out.println("i = " + i);
-        }
+public class WebApplicationServer {
+
+    private static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
+
+    public static void main(String[] args) throws Exception {
+        String webappDirLocation = "webapps/";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        log.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
---
+++ oop-practice/src/main/java/org/example/PasswordValidator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class PasswordValidator {
+
+    public static final String Wrong_Password_length_exception_message = "비밀번호는 최소 8자 이상 12자 이하여야 합니다.";
+
+    public static void validate(String password){
+        int length = password.length();
+
+        if (length < 8 || length > 12){
+            throw new IllegalArgumentException(Wrong_Password_length_exception_message);
+        }
+    }
+}
